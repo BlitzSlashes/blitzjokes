97b7c2416572bd1647854075fcdab44d283fc3fb
BlitzSlashes <63169496+BlitzSlashes@users.noreply.github.com>
2020-07-17 21:55:10 +0300

    Updated the code a little bit
    Made it more readable and better for future updates

--- blitzjokes.py
+++ blitzjokes.py
@@ -12,20 +12,45 @@ def search(ctx, limit):
     return jokes
 
 
+def generate_jokes():
+    view = 'yes'
+    while view == 'yes':
+        ctx = input('Enter a keyword for the jokes: ').lower()
+        count = input('How many jokes do you want?: ')
+        if not count:
+            count = 5
+        res = search(ctx, count)
+        if len(res):
+            print(
+                colored(f'Okay there were {len(res)} jokes about {ctx}...', color='green'))
+            data = "\n".join(res)
+            print(colored(data, color='yellow'))
+            view = input(
+                'Would you like to continue browsing the jokes?(yes/no): ')
+        else:
+            print(
+                colored(f"Uh oh there weren't any jokes containing {ctx}", color='red'))
+            view = input('Would you like to try again?(yes/no): ')
+
+
+def options():
+    options = ["getjokes"]
+    return options
+
+
+def selectOption(option):
+    selection = {
+        'getJokes': generate_jokes()
+    }
+    return selection.get(option)
+
+
 def main():
-    print(colored(pyfiglet.figlet_format("Blitz jokes 1.0.0"), color='red'))
-    ctx = input('Enter a keyword for the jokes: ').lower()
-    count = input('How many jokes do you want?: ')
-    if not count:
-        count = 5
-    res = search(ctx, count)
-    if len(res):
-        print(
-            colored(f'Okay there were {len(res)} jokes about {ctx}...', color='green'))
-        data = "\n".join(res)
-        return colored(data, color='yellow')
-    else:
-        return colored(f"Uh oh there weren't any jokes containing {ctx}", color='red')
+    print(colored(pyfiglet.figlet_format("Blitz jokes 1.0.1"), color='red'))
+    option = input(colored(
+        f'HELLO THERE! What would you like to do?{options()}: ', color='yellow'))
+    selectOption(option)
+    return colored('Thanks for using Blitz jokes!', color='green')
 
 
 print(main())
